139cfb36c8e476c6d456e3d0d88f7f22a84c0770
futuroptimist <80668851+futuroptimist@users.noreply.github.com>
2025-09-23 23:45:30 -0700

    📝 : – restore original post prose

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  prefetch: {
+    defaultStrategy: 'viewport',
+  },
+  markdown: {
+    syntaxHighlight: 'shiki',
+    shikiConfig: {
+      theme: 'dracula-soft',
+    },
+  },
+});
+
